9dfbb21cc789d40c1e816a39aec769b1ec15f8ec
pradeepkumaresan <pradeepk@thoughtworks.com>
2023-06-24 15:46:08 +0530

    changes

--- xss/1/script.js
+++ xss/1/script.js
@@ -6,3 +6,12 @@ if (params.search) {
   document.write(search);
   //   document.getElementById("output").append(document.createRange().createContextualFragment(search));
 }
+document.write('hello world')
+document.write('devkode')
+
+// write a function named 'click' which prints hello via document.write
+// and attach it to the button with id 'click'
+function click() {
+  document.write('hello')
+}
+click();
--- xss/2/script.js
+++ xss/2/script.js
@@ -2,7 +2,7 @@ const searchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(searchParams);
 
 if (params.search) {
-  const input = params.search.replaceAll("script", "");
+  const input = params.search.toLowerCase().replaceAll("script", "");
   const search = '<h4>No results found for "' + input +'"</h4>'
   // const search = params.search.toLowerCase().replaceAll("script", "");
   // document.write(search);
